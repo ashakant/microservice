ba420a60f5f98f8dc1eed86807fdc9b8c2a022c3
ashakant <ashakant.kumar@gmail.com>
2018-02-18 14:48:12 +0530

    commit/undo

---
+++ useraccount/src/main/java/com/ashakant/useraccount/Config.java
@@ -0,0 +1,11 @@
+package com.ashakant.useraccount;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.ashakant.useraccount")
+@Configuration
+public class Config {
+
+
+}
